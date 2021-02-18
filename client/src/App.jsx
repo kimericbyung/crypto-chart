296847984eeb8abc3a9ee1af812dfb092df27d0d
Eric Kim <kimericbyung@gmail.com>
2021-02-18 13:40:57 -0800

    Better separation of concerns

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import Chart from 'chart.js';
+import createGraph from './createGraph'
 
 class App extends React.Component {
   constructor(props) {
@@ -12,55 +12,17 @@ class App extends React.Component {
   componentDidMount() {
     axios.get(this.props.btcHTML)
       .then(res => {
-        // console.log(Object.keys(res.data.bpi));
         this.makeGraph(res.data.bpi);
       })
   }
 
   makeGraph(data) {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: Object.keys(data),
-        datasets: [{
-          label: '# of Votes',
-          data: Object.values(data),
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
+    createGraph(Object.keys(data), Object.values(data));
   }
 
   render() {
     return (
       <div>
-        Bitcoin Prices
       </div>
     )
   }
